0806d06aee37d323b4b8ddbf7339f352001810e2
Lys <874907806@qq.com>
2017-12-12 12:18:48 +0800

    Initial Commit

---
+++ swiftSimpleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftSimpleProject
+//
+//  Created by 李燕山 on 2017/12/12.
+//  Copyright © 2017年 tests. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
